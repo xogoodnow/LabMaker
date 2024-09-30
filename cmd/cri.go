55a8b134fcd5ced6dc0c744dd433cfc550e4dbc6
xogoodnow <xo4ever9999@gmail.com>
2024-09-30 21:25:25 +0330

    Forgot to update

--- cmd/cri.go
+++ cmd/cri.go
@@ -8,12 +8,15 @@ import (
 
 func InstallDocker() {
 	cmds := []string{
+		"sudo apt update",
 		"sudo apt install -y apt-transport-https ca-certificates curl software-properties-common",
 		"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
 		"sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable'",
+		"sudo apt update",
 		"apt-cache policy docker-ce",
-		"sudo apt install -y docker-ce",
-		"sudo systemctl status docker",
+		"sudo apt install -y docker-ce docker-ce-cli containerd.io",
+		"sudo systemctl enable docker",
+		"sudo systemctl start docker",
 	}
 	runCommands(cmds)
 
